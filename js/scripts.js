30756e36b2e2eec566a4e5e8020ad8e610b297fa
ErnLance <80061275+ErnLance@users.noreply.github.com>
2021-03-21 19:57:50 +0100

    all

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
-/* <script>
-  alert("Hello");
-</script> example for html and JS comments */
+//This is a single line comment, started with two slashes ¿AND END WITH DOT COMA?;
+// <script>  alert("Hello");</script> (example for html command for JS);
 
 
 
