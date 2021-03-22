616c668a79c4578548a272e6a92bec870f4890bc
ErnLance <80061275+ErnLance@users.noreply.github.com>
2021-03-22 22:25:54 +0100

    all
    if applied, this commit will set JS burger Icon, with error

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,12 @@
 
 
 console.log("Welcome to my portfolio!!")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
