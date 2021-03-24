f7b72bf9aef5b07aa70c62e09031f95c34d2548f
ErnLance <80061275+ErnLance@users.noreply.github.com>
2021-03-24 21:07:23 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // This is a single line comment, started with two slashes ¿AND END WITH DOT COMA?;
 // <script>  alert("Hello");</script> (example for html command for JS);
 
-console.log("Welcome to my portfolio!!")
+console.log('Welcome to my portfolio!!')
 
 function menuToggle () {
   var x = document.getElementById('myNavtoggle')
